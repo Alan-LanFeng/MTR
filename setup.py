8df2683317becc7b2f5bff526e48aaed3608d42c
Alan <fenglan18@outlook.com>
2023-12-18 15:28:50 +0100

    update setup

--- setup.py
+++ setup.py
@@ -32,8 +32,8 @@ def write_version_to_file(version, target_file):
 
 
 if __name__ == '__main__':
-    version = '0.1.0+%s' % get_git_commit_number()
-    write_version_to_file(version, 'mtr/version.py')
+    # version = '0.1.0+%s' % get_git_commit_number()
+    # write_version_to_file(version, 'mtr/version.py')
 
     setup(
         name='MotionTransformer',
